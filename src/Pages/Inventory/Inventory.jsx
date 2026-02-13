7791b09f1f2ba336aeeea88e7d01bc4389b288e4
Andy Bandela <andybandela@gmail.com>
2026-02-12 21:14:23 -0500

    modify Invetory page styling

--- src/Pages/Inventory/Inventory.jsx
+++ src/Pages/Inventory/Inventory.jsx
@@ -1,79 +1,101 @@
 import React from 'react'
 import './Inventory.css'
+import plusIcn from '../../assets/plus.png'
+import plusIng from '../../assets/add.png'
+import minusIng from '../../assets/minus.png'
+import dwnChev from '../../assets/downchevron.png'
+import upChev from '../../assets/upchevron.png'
 
 const Inventory = () => {
   return (
     <div className='inventory-page'>
         <div className='inventory-layout'>
             <div className='inventory-main'>
-                <h1 className='inventory-title'>My Inventory</h1>
+                
                 <div className='inventory-paper'>
+                    <h1 className='inventory-title'>My Inventory</h1>
                     <div className='category-block'>
-                        <div className='category-head'>
+                        <div className='category-header'>
                             <h3>Protein</h3>
-                            <button className='add-btn'>+</button>
-                            <button className='collapse-btn'>⌄</button>
+                            <div>
+                                <button > <img className='add-btn' src={plusIcn}/> </button>
+                                <button ><img className='collapse-btn' src={upChev}/></button>
+                            </div>
+                            
                         </div>
                         <div className='ingredient-row'>
                             <span>Chicken</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>2</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
                             </div>
                         </div>
                         <div className='ingredient-row'>
                             <span>Beef</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>2</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
                             </div>
                         </div>
                     </div>
                     <div className='category-block'>
-                        <div className='category-head'>
+                        <div className='category-header'>
                             <h3>Produce</h3>
-                            <button className='add-btn'>+</button>
-                            <button className='collapse-btn'>⌄</button>
+                            <div>
+                                <button > <img className='add-btn' src={plusIcn}/> </button>
+                                <button ><img className='collapse-btn' src={upChev}/></button>
+                            </div>
                         </div>
                         <div className='ingredient-row'>
                             <span>Cabbage</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>1</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
                             </div>
                         </div>
                         <div className='ingredient-row'>
                             <span>Tomato</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>4</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
                             </div>
                         </div>
                     </div>
                     <div className='category-block'>
-                        <div className='category-head'>
+                        <div className='category-header'>
                             <h3>Seasoning & Herb</h3>
-                            <button className='add-btn'>+</button>
-                            <button className='collapse-btn'>⌄</button>
+                            <div>
+                                <button > <img className='add-btn' src={plusIcn}/> </button>
+                                <button ><img className='collapse-btn' src={upChev}/></button>
+                            </div>
                         </div>
                         <div className='ingredient-row'>
                             <span>Black Pepper</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>1</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
                             </div>
                         </div>
                         <div className='ingredient-row'>
                             <span>Nutmeg</span>
                             <div className='quantity-controls'>
-                                <button className='qty-btn'>-</button>
+                                <button ><img className='qty-btn' src={minusIng}/></button>
                                 <span className='qty-number'>1</span>
-                                <button className='qty-btn'>+</button>
+                                <button ><img className='qty-btn' src={plusIng}/></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='category-block'>
+                        <div className='category-header'>
+                            <h3>Dairy</h3>
+                            <div>
+                                <button > <img className='add-btn' src={plusIcn}/> </button>
+                                <button ><img className='collapse-btn' src={dwnChev}/></button>
                             </div>
                         </div>
                     </div>
@@ -94,6 +116,9 @@ const Inventory = () => {
         <div className='recommendation-section'>
                 <h2>Recommended Recipes</h2>
                 <div className='recommendation-cards'>
+                    <div className='rec-card'>Recipe</div>
+                    <div className='rec-card'>Recipe</div>
+                    <div className='rec-card'>Recipe</div>
                     <div className='rec-card'>Recipe</div>
                     <div className='rec-card'>Recipe</div>
                     <div className='rec-card'>Recipe</div>
